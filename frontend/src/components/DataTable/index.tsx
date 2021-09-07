57912f54dfea88841a39567811092ea99c05af73
João Paulo <jpsilva.math@icloud.com>
2021-09-07 15:48:45 -0400

    Basic static components

---
+++ frontend/src/components/DataTable/index.tsx
@@ -0,0 +1,100 @@
+
+const DataTable = () =>{
+    return(
+        <div className="table-responsive">
+    <table className="table table-striped table-sm">
+        <thead>
+            <tr>
+                <th>Data</th>
+                <th>Vendedor</th>
+                <th>Clientes visitados</th>
+                <th>Negócios fechados</th>
+                <th>Valor</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>22/04/2021</td>
+                <td>Barry Allen</td>
+                <td>34</td>
+                <td>25</td>
+                <td>15017.00</td>
+            </tr>
+            <tr>
+                <td>22/04/2021</td>
+                <td>Maria Clara</td>
+                <td>34</td>
+                <td>25</td>
+                <td>15017.00</td>
+            </tr>
+            <tr>
+                <td>22/04/2021</td>
+                <td>Fernando Allen</td>
+                <td>34</td>
+                <td>25</td>
+                <td>15017.00</td>
+            </tr>
+            <tr>
+                <td>22/04/2021</td>
+                <td>Helena Santos</td>
+                <td>34</td>
+                <td>25</td>
+                <td>15017.00</td>
+            </tr>
+            <tr>
+                <td>22/04/2021</td>
+                <td>Miguel Nunes</td>
+                <td>34</td>
+                <td>25</td>
+                <td>15017.00</td>
+            </tr>
+            <tr>
+                <td>22/04/2021</td>
+                <td>Jose Fernando</td>
+                <td>34</td>
+                <td>25</td>
+                <td>15017.00</td>
+            </tr>
+            <tr>
+                <td>22/04/2021</td>
+                <td>Letícia Lima</td>
+                <td>34</td>
+                <td>25</td>
+                <td>15017.00</td>
+            </tr>
+            <tr>
+                <td>22/04/2021</td>
+                <td>Izabel Allen</td>
+                <td>34</td>
+                <td>25</td>
+                <td>15017.00</td>
+            </tr>
+            <tr>
+                <td>22/04/2021</td>
+                <td>Paulo Allen</td>
+                <td>34</td>
+                <td>25</td>
+                <td>15017.00</td>
+            </tr>
+            <tr>
+                <td>22/04/2021</td>
+                <td>Jussara Allen</td>
+                <td>34</td>
+                <td>25</td>
+                <td>15017.00</td>
+            </tr>
+            <tr>
+                <td>22/04/2021</td>
+                <td>Tadeu Allen</td>
+                <td>34</td>
+                <td>25</td>
+                <td>15017.00</td>
+            </tr>
+        </tbody>
+    </table>
+</div>
+
+    );
+}
+
+export default DataTable;
---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,17 @@
+
+const Footer = () => {
+    return (
+
+<footer className="footer mt-auto py-3 bg-dark">
+  <div className="container">
+    <p className="text-light">App desenvolvido por <a href="https://github.com/JPGSilva" target="_blank" rel="noreferrer">João Paulo</a></p>
+    <p className="text-light"><small><strong>Semana Spring React</strong><br/>
+  Por João Paulo Desen. Web: <a href="https://www.instagram.com/joao_paulo_gsilva/" target="_blank" rel="noreferrer">@joao_paulo_gsilva</a></small></p>
+    </div>
+  </footer>
+  
+  );
+  
+  }
+  
+  export default Footer;
